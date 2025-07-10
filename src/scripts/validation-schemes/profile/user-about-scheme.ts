6a17fad619aa64a96987f259fa618d2d3320ac00
Volodymyr Konopl <konopl538@gmail.com>
2025-07-10 17:32:24 +0300

    Update user-about scheme.
    Make username to contain only english letters or digits.

--- src/scripts/validation-schemes/profile/user-about-scheme.ts
+++ src/scripts/validation-schemes/profile/user-about-scheme.ts
@@ -1,12 +1,23 @@
 import { z } from 'zod';
 
+function containsOnlyEnglishOrDigits(str: string) {
+  const englishLettersOrDigitsRegex = /^[a-zA-Z0-9]+$/;
+  return englishLettersOrDigitsRegex.test(str);
+}
+
+function containsOnlyLetters(str: string) {
+  const letterRegex = /^\p{L}+$/u;
+  return letterRegex.test(str);
+}
+
 const nameString = z
   .string({ message: 'Must be a string.' })
   .min(2, 'Minimum 2 characters.')
   .max(35, 'Maximum 35 characters.')
-  .regex(/^[a-zA-Z]+$/, {
-    message: 'Name can only contain letters',
-  })
+  .refine(
+    (value: string) => containsOnlyLetters(value),
+    'Can contain only letters.',
+  )
   .refine((value: string) => !value.includes(' '), 'You can not use spaces.')
   .transform(
     (str: string) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase(),
@@ -21,6 +32,10 @@ export const userAboutScheme = z.object({
     .refine(
       (value: string) => value.length === 0 || value.length >= 4,
       'Minimum 4 characters (or can be empty).',
+    )
+    .refine(
+      (value: string) => containsOnlyEnglishOrDigits(value),
+      'Only English letters or digits are allowed.',
     )
     .refine(
       (value: string | undefined) => !value || !value.includes(' '),
