a4f9c830b2fd00892958bdc03d8ef6733b21f517
Volodymyr Konopl <konopl538@gmail.com>
2025-07-12 23:08:03 +0300

    Add unit tests for levenshtein distance function.

---
+++ src/__tests__/levenstein-distance.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from 'vitest';
+import { levenshteinDistance } from '@/scripts/levenshtein-distance';
+
+test('Empty strings', () => {
+  expect(levenshteinDistance('', '')).toBe(0);
+});
+
+test('Similar words', () => {
+  expect(levenshteinDistance('apple', 'apple')).toBe(0);
+});
+
+test('Single insertion', () => {
+  expect(levenshteinDistance('aple', 'apple')).toBe(1);
+});
+
+test('Single deletion', () => {
+  expect(levenshteinDistance('appple', 'apple')).toBe(1);
+});
+
+test('Single replacement', () => {
+  expect(levenshteinDistance('aprle', 'apple')).toBe(1);
+});
+
+test('Empty string to "apple"', () => {
+  expect(levenshteinDistance('', 'apple')).toBe(5);
+});
+
+test('"apple" to "applesauce"', () => {
+  expect(levenshteinDistance('apple', 'applesauce')).toBe(5);
+});
+
+test('"apple" to "alphabet"', () => {
+  expect(levenshteinDistance('apple', 'alphabet')).toBe(5);
+});
+
+test('"apple" to "archer"', () => {
+  expect(levenshteinDistance('apple', 'archer')).toBe(4);
+});
--- src/scripts/levenshtein-distance.ts
+++ src/scripts/levenshtein-distance.ts
@@ -1,4 +1,4 @@
-export function levenshteinDistance(
+function levenshteinDistanceRecurse(
   str1: string,
   str2: string,
   m: number, // idx for str1
@@ -9,15 +9,20 @@ export function levenshteinDistance(
 
   // no operations required
   if (str1[m - 1] === str2[n - 1]) {
-    return levenshteinDistance(str1, str2, m - 1, n - 1);
+    return levenshteinDistanceRecurse(str1, str2, m - 1, n - 1);
   }
 
   return (
     1 +
     Math.min(
-      levenshteinDistance(str1, str2, m, n - 1), // insertion
-      levenshteinDistance(str1, str2, m - 1, n), // deletion
-      levenshteinDistance(str1, str2, m - 1, n - 1), // replacement
+      levenshteinDistanceRecurse(str1, str2, m, n - 1), // insertion
+      levenshteinDistanceRecurse(str1, str2, m - 1, n), // deletion
+      levenshteinDistanceRecurse(str1, str2, m - 1, n - 1), // replacement
     )
   );
+}
+
+// Note: case-sensitive
+export function levenshteinDistance(str1: string, str2: string) {
+  return levenshteinDistanceRecurse(str1, str2, str1.length, str2.length);
 }
